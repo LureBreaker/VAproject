5492456ccfe8c9ab9ddea5531eaf3472fc0dfdb2
Lim Xian Ming <xmlim.2011@sis.smu.edu.sg>
2014-11-16 20:01:08 +0800

    first draft

---
+++ js/record.js
@@ -0,0 +1,352 @@
+number_visitor_data = 
+ [
+ {
+    "key": "2009",
+    "color": "#d67777",
+    "values": [
+      { 
+        "label" : "Indonesia" ,
+        "value" : 1745330
+      } , 
+      { 
+        "label" : "PRChina" ,
+        "value" : 936747
+      } , 
+      { 
+        "label" : "Malaysia" ,
+        "value" : 764309
+      } , 
+      { 
+        "label" : "Australia" ,
+        "value" : 830299
+      } , 
+      {
+        "label" : "India" ,
+        "value" : 725624
+      } , 
+      { 
+        "label" : "Japan" ,
+        "value" : 489987
+      } ,      
+      {
+        "label" : "Philippines" ,
+        "value" : 432072
+      } , 
+	  { 
+        "label" : "UK" ,
+        "value" : 469756
+      } ,
+	  { 
+        "label" : "USA" ,
+        "value" : 370704
+      } ,
+      { 
+        "label" : "Thailand" ,
+        "value" : 317905
+      }  
+      
+    ]
+  },{
+    "key": "2010",
+    "color": "#d67777",
+    "values": [
+      { 
+        "label" : "Indonesia" ,
+        "value" : 2306243
+      } , 
+      { 
+        "label" : "PRChina" ,
+        "value" : 1171493
+      } , 
+      { 
+        "label" : "Malaysia" ,
+        "value" : 1037489
+      } , 
+      { 
+        "label" : "Australia" ,
+        "value" : 880558
+      } , 
+      {
+        "label" : "India" ,
+        "value" : 828994
+      } , 
+      { 
+        "label" : "Japan" ,
+        "value" : 528951
+      } , 
+	  {
+        "label" : "Philippines" ,
+        "value" : 544449
+      } ,
+      { 
+        "label" : "UK" ,
+        "value" : 461769
+      } ,
+	  { 
+        "label" : "USA" ,
+        "value" : 417195
+      },       
+      { 
+        "label" : "Thailand" ,
+        "value" : 430067
+      } 
+    ]
+  },{
+    "key": "2011",
+    "color": "#d67777",
+    "values": [
+      { 
+        "label" : "Indonesia" ,
+        "value" : 2592222
+      } , 
+      { 
+        "label" : "PRChina" ,
+        "value" : 1577522
+      } , 
+      { 
+        "label" : "Malaysia" ,
+        "value" : 1140935
+      } , 
+      { 
+        "label" : "Australia" ,
+        "value" : 956039
+      } , 
+      {
+        "label" : "India" ,
+        "value" : 868991
+      } , 
+      { 
+        "label" : "Japan" ,
+        "value" : 656417
+      } , 
+	  {
+        "label" : "Philippines" ,
+        "value" : 677723
+      },
+      { 
+        "label" : "UK" ,
+        "value" : 442611
+      } , 
+	  { 
+        "label" : "USA" ,
+        "value" : 440576
+      } ,
+      { 
+        "label" : "Thailand" ,
+        "value" : 472708
+      }     
+    ]
+  },{
+    "key": "2012",
+    "color": "#d67777",
+    "values": [
+      { 
+        "label" : "Indonesia" ,
+        "value" : 2837537
+      } , 
+      { 
+        "label" : "PRChina" ,
+        "value" : 2034177
+      } , 
+      { 
+        "label" : "Malaysia" ,
+        "value" : 1231686
+      } , 
+      { 
+        "label" : "Australia" ,
+        "value" : 1050373
+      } , 
+      {
+        "label" : "India" ,
+        "value" : 894993
+      } , 
+      { 
+        "label" : "Japan" ,
+        "value" : 757116
+      } , {
+        "label" : "Philippines" ,
+        "value" : 656804
+      } ,
+      { 
+        "label" : "UK" ,
+        "value" : 446497
+      } , 
+       { 
+        "label" : "USA" ,
+        "value" : 477213
+      } ,
+      { 
+        "label" : "Thailand" ,
+        "value" : 477654
+      }  
+      
+    ]
+  },{
+    "key": "2013",
+    "color": "#d67777",
+    "values": [
+      { 
+        "label" : "Indonesia" ,
+        "value" : 3088859
+      } , 
+      { 
+        "label" : "PRChina" ,
+        "value" : 2269870
+      } , 
+      { 
+        "label" : "Malaysia" ,
+        "value" : 1280942
+      } , 
+      { 
+        "label" : "Australia" ,
+        "value" : 1125179
+      } , 
+      {
+        "label" : "India" ,
+        "value" : 933553
+      } , 
+      { 
+        "label" : "Japan" ,
+        "value" : 832845
+      } , {
+        "label" : "Philippines" ,
+        "value" : 687794
+      },
+      { 
+        "label" : "UK" ,
+        "value" : 461459
+      } ,  
+	  { 
+        "label" : "USA" ,
+        "value" : 491946
+      } ,
+      { 
+        "label" : "Thailand" ,
+        "value" : 497409
+      }  
+      
+    ]
+  }
+ 
+]
+;
+
+long_short_data2 = 
+ [
+  {
+    "key": "Number of visitors",
+    "color": "#d67777",
+    "values": [
+      { 
+        "label" : "Indonesia" ,
+        "value" : 20
+      } , 
+      { 
+        "label" : "P R China" ,
+        "value" : 12
+      } , 
+      { 
+        "label" : "Malaysia" ,
+        "value" : 78
+      }  , 
+      { 
+        "label" : "Malaysia" ,
+        "value" : 78
+      }, 
+      { 
+        "label" : "Australia" ,
+        "value" : 71
+      } , 
+      {
+        "label" : "India" ,
+        "value" : 63
+      } , 
+      { 
+        "label" : "Japan" ,
+        "value" : 54
+      } , 
+      { 
+        "label" : "Hong Kong SAR" ,
+        "value" : 46
+      } , 
+      {
+        "label" : "South Korea" ,
+        "value" : 37
+      } , 
+      { 
+        "label" : "Thailand" ,
+        "value" : 34
+      } , 
+      { 
+        "label" : "USA" ,
+        "value" : 33
+      } 
+    ]
+  }
+]
+;
+
+var testdata = [
+    {
+      key: "One",
+      y: 5
+    },
+    {
+      key: "Two",
+      y: 2
+    },
+    {
+      key: "Three",
+      y: 9
+    }
+  ];
+
+
+var transport2012 = [
+    {
+        "Indonesia": [
+            {
+                "key": "Air",
+                "y": 1752591
+            },
+            {
+                "key": "Sea",
+                "y": 259075
+            },
+            {
+                "key": "Land",
+                "y": 825871
+            }
+        ]
+    }
+];  
+
+var Indonesia2012 = [
+    {
+      key: "Air",
+      y: 1752591
+    },
+    {
+      key: "Sea",
+      y: 825871
+    },
+    {
+      key: "Land",
+      y: 259075
+    }
+  ];
+  
+var PRChina2012 = [
+    {
+      key: "Air",
+      y: 1455978
+    },
+    {
+      key: "Sea",
+      y: 62352
+    },
+    {
+      key: "Land",
+      y: 515847
+    }
+  ];
